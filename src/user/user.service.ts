c94d7072a3663799589e1087e3218cceeaf8bb7b
Ceban Octavian <oktav2222@gmail.com>
2025-04-27 00:17:06 +0200

    hashPass

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,21 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { prismaErrors } from 'prisma/errors';
 import { PrismaService } from 'src/prisma.service';
 import { CreateUserDto } from './dto/createUser.dto';
-
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+
+  private readonly saltRounds = 11;
 
   private readonly userSelect = {
     id: true,
     email: true,
     name: true,
     createdAt: true,
+    password: true,
   };
 
   private handlePrismaError(error: any): never {
@@ -37,9 +40,13 @@ export class UserService {
     if (data.password.length < 6)
       throw new BadRequestException('Password is Required');
 
+    const { password, ...restData } = data;
+
+    const hashedPassword = await bcrypt.hash(password, this.saltRounds);
+
     try {
       return await this.prisma.user.create({
-        data,
+        data: { ...restData, password: hashedPassword },
         select: this.userSelect,
       });
     } catch (error) {
