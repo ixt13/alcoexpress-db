e25d4f47ad431894005929aea1cb6011b0b5a43a
Ceban Octavian <oktav2222@gmail.com>
2025-05-24 10:48:41 +0200

    fx

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CartItemModule } from './cart-item/cartItem.module';
 import { ItemModule } from './item/item.module';
 import { UserModule } from './user/user.module';
-import { CartItemModule } from './cart-item/cartItem.module';
 
 @Module({
   imports: [UserModule, ConfigModule.forRoot(), ItemModule, CartItemModule],
